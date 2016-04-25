c27bfc679f25b355c75e7ea93b479985a40b8e34
caroso1222 <ce.roso398@gmail.com>
2016-04-25 00:52:42 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    livereload = require('gulp-livereload'),
+    webserver = require('gulp-webserver'),
+    del = require('del');
+    concat = require('gulp-concat');
+
+
+gulp.task('styles', function() {
+  return sass('src/css/scss/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+  console.log("run scripts");
+  return gulp.src('src/js/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/imgs/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/imgs'));
+});
+
+gulp.task('htmls', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean', function() {
+    return del(['dist/css', 'dist/js', 'dist/img']);
+});
+
+gulp.task('build', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'fonts', 'htmls');
+});
+
+gulp.task('watch', function() {
+
+	// Watch .scss files
+  gulp.watch('src/css/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/imgs/**/*', ['images']);
+
+
+  gulp.watch('src/fonts/*', ['fonts']);
+
+
+  gulp.watch('src/**/*.html', ['htmls']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
+
+gulp.task('serve', ['webserver', 'watch']);
+
+
+gulp.task('webserver', function() {
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      port:2000
+    }));
+});
+
+
+
+
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+var app = angular.module('app',[]);
+
+app.controller('mainController',function($scope){
+	$scope.functionalities={};
+	$scope.functionalities.items=[
+	{'description':'Página de bienvenida', 'hours':10, 'selected':false},
+	{'description':'Registro y Log-In con correo', 'hours':8, 'selected':false},
+	{'description':'Registro y Log-In con Facebook', 'hours':15, 'selected':false}
+	];
+
+	$scope.totalHours = function(){
+		var sum = 0; 
+		$scope.functionalities.items.forEach(function(elem,idx,array){
+			sum += elem.selected ? elem.hours : 0;
+		});
+		return sum;
+	}
+});
