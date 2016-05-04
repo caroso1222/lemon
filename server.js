3604f9f2d956ff93f6f704e560e57e0c4f991989
Carlos <ce.roso398@gmail.com>
2016-05-04 13:39:50 -0500

    Connected to DBgit add . :D

--- server.js
+++ server.js
@@ -2,19 +2,44 @@ var config = require('./config');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 //Setting up database connection on mlab
-var mongoose = require('mongoose');
+console.log(config);
 var mongoUri = 'mongodb://'+config.dbUser+':'+config.dbPassword+'@ds061777.mlab.com:61777/'+config.dbName;
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };    
+var options = { server: { socketOptions: { keepAlive: 3000, connectTimeoutMS: 3000 } }, 
+                replset: { socketOptions: { keepAlive: 3000, connectTimeoutMS : 3000 } } };
+
+
+console.log(mongoUri);
 mongoose.connect(mongoUri,options);
 
-var conn = mongoose.connection;
-conn.on('error', console.error.bind(console, 'connection error:'));  
-conn.once('open', function() {
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));  
+db.once('open', function() {
   // Wait for the database connection to establish, then start the app.     
   console.log("its ready db");
+  var testSchema = mongoose.Schema({
+  	name: String
+  });
+  testSchema.methods.speak = function(){
+  	var greeting = this.name ? "My name is " + this.name : "I dont have a name";
+  	console.log(greeting);
+  }
+  var MyModel = mongoose.model('MyModel',testSchema);
+  var carlos = new MyModel({name:"Carlos"});
+  console.log(carlos.name);
+  carlos.speak();
+  carlos.save(function(err, carlos){
+  	if(err){
+  		return console.log(err);
+  	}
+  	carlos.speak();
+  });
+  MyModel.find(function(err, docs){
+  	if(err) return console.log(err);
+  	console.log(docs); 
+  });
 });
 
 app.use(express.static(__dirname + "/public"));
