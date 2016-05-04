89b662aca3392a18024835ac5c03cba7384e3243
caroso1222 <ce.roso398@gmail.com>
2016-05-04 00:15:06 -0500

    Polished and fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,26 @@ var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
 var livereload = require('gulp-livereload');
+var uglify = require('gulp-uglify');
  
 gulp.task('serve', ['server','watch']);
 
 gulp.task('server',function(){
-    // Start the server at the beginning of the task 
-    nodemon({'script': 'server.js'});
+    nodemon({
+        'script': 'server.js',
+        'ignore': 'public/js/*.js'
+    });
 });
 
 gulp.task('styles', function() {
-  return sass('public/css/scss/main.scss', { style: 'expanded' })
+  return sass('public/css/scss/*.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('public/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(cssnano())
-    .pipe(gulp.dest('public/css'))
+    //.pipe(rename({suffix: '.min'}))
+    //.pipe(cssnano())
+    //.pipe(gulp.dest('public/css'))
     .pipe(livereload());
 });
 
@@ -28,15 +32,21 @@ gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('public/css/**/*.scss', ['styles']);
     gulp.watch('public/js/*.js', ['scripts']);
+    gulp.watch('views/**/*.ejs', ['ejs']);
 });
 
+gulp.task('ejs',function(){
+    return gulp.src('views/**/*.ejs')
+    .pipe(livereload());
+});
 
 gulp.task('scripts', function() {
-  return gulp.src('public/js/main.js')
+  return gulp.src('public/js/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
-    .pipe(gulp.dest('public/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/js'));
+    //.pipe(gulp.dest('public/js'))
+    //.pipe(rename({suffix: '.min'}))
+    //.pipe(uglify())
+    //.pipe(gulp.dest('public/js'))
+    .pipe(livereload());
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var app = angular.module('app',[]);
 
 app.controller('mainController',function($scope){
